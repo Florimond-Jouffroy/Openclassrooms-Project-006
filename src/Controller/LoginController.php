16e141633aef481b30e3bb05323615aeb0328814
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2022-04-25 16:48:29 +0200

    feat: Reset Password 2/2 #6

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,29 +2,40 @@
 
 namespace App\Controller;
 
-use App\Entity\PasswordRequest;
 use App\Form\LoginType;
 use App\Form\EmailRequestType;
-use App\Repository\PasswordRequestRepository;
+use App\Service\MailerService;
+use App\Entity\PasswordRequest;
+use App\Form\PasswordRequestType;
 use App\Repository\UserRepository;
+use Symfony\Component\Mime\Address;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\PasswordRequestRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class LoginController extends AbstractController
 {
 
-  private $userRepository, $passwordRequestRepository, $em;
+  private $userRepository, $passwordRequestRepository, $em, $mailer, $passwordHasher;
 
-  public function __construct(UserRepository $userRepository, PasswordRequestRepository $passwordRequestRepository, EntityManagerInterface $em)
-  {
+  public function __construct(
+    UserRepository $userRepository,
+    PasswordRequestRepository $passwordRequestRepository,
+    EntityManagerInterface $em,
+    MailerService $mailer,
+    UserPasswordHasherInterface $userPasswordHasher
+  ) {
     $this->userRepository = $userRepository;
     $this->passwordRequestRepository = $passwordRequestRepository;
     $this->em = $em;
+    $this->mailer = $mailer;
+    $this->passwordHasher = $userPasswordHasher;
   }
 
   #[Route('/login', name: 'user_login')]
@@ -77,15 +88,57 @@ class LoginController extends AbstractController
 
       $url = $this->generateUrl('user_resetPassword', ['token' => $passwordRequest->getToken(), UrlGeneratorInterface::ABSOLUTE_URL]);
 
-      // Construction de notre email
 
-      // Envoie de notre email
+      $emailParameters = array(
+        'from' => 'noreply@snowtrick.com',
+        'to' => new Address($userEmail),
+        'subject' => "Reset Password",
+        'htmlTemplate' => 'email/resetPassword.html.twig',
+      );
+
+      $contextParameters = array(
+        'url' => $url,
+      );
+
+      $this->mailer->send($emailParameters, $contextParameters);
 
       return $this->redirectToRoute('home');
     }
 
     return $this->render('user/password/emailRequest.html.twig', [
       'form' => $form
+    ]);
+  }
+
+
+  #[Route('/reset-password/{token}', name: "user_resetPassword")]
+  public function resetPassword(Request $request, string $token)
+  {
+    $passwordRequest = $this->passwordRequestRepository->findOneBy(['token' => $token]);
+
+    $form = $this->createForm(PasswordRequestType::class);
+    $form->handleRequest($request);
+
+    if ($passwordRequest === null) {
+      return $this->redirectToRoute('home');
+    }
+
+    if ($form->isSubmitted() && $form->isValid()) {
+
+      $newPassword = $form->getData('password');
+      $user = $this->userRepository->findOneBy(['email' => $passwordRequest->getEmail()]);
+
+      $user->setPassword($this->passwordHasher->hashPassword($user, $newPassword['password']));
+
+      $this->em->persist($user);
+      $this->em->remove($passwordRequest);
+      $this->em->flush();
+
+      return $this->redirectToRoute('home');
+    }
+
+    return $this->render('user/password/passwordRequest.html.twig', [
+      'form' => $form->createView()
     ]);
   }
 }
