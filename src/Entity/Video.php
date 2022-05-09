b765ec75aa45fb58da482286d3b8816457064091
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2022-05-09 14:44:27 +0200

    feat: Video Link #10

--- src/Entity/Video.php
+++ src/Entity/Video.php
@@ -6,45 +6,56 @@ use App\Repository\VideoRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: VideoRepository::class)]
+#[ORM\HasLifecycleCallbacks()]
 class Video
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
-    private $id;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column(type: 'integer')]
+  private $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private $link;
+  #[ORM\Column(type: 'string', length: 255)]
+  private $link;
 
-    #[ORM\ManyToOne(targetEntity: Trick::class, inversedBy: 'videos')]
-    private $trick;
+  #[ORM\ManyToOne(targetEntity: Trick::class, inversedBy: 'videos')]
+  private $trick;
 
-    public function getId(): ?int
-    {
-        return $this->id;
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getLink(): ?string
+  {
+    return $this->link;
+  }
+
+  public function setLink(string $link): self
+  {
+    $this->link = $link;
+
+    return $this;
+  }
+
+  public function getTrick(): ?Trick
+  {
+    return $this->trick;
+  }
+
+  public function setTrick(?Trick $trick): self
+  {
+    $this->trick = $trick;
+
+    return $this;
+  }
+
+  #[ORM\PrePersist()]
+  #[ORM\PreUpdate()]
+  public function test()
+  {
+
+    if (preg_match('/src="(?<embedUrl>[^"]+)"/', $this->getLink(), $out)) {
+      $this->setLink($out['embedUrl']);
     }
-
-    public function getLink(): ?string
-    {
-        return $this->link;
-    }
-
-    public function setLink(string $link): self
-    {
-        $this->link = $link;
-
-        return $this;
-    }
-
-    public function getTrick(): ?Trick
-    {
-        return $this->trick;
-    }
-
-    public function setTrick(?Trick $trick): self
-    {
-        $this->trick = $trick;
-
-        return $this;
-    }
+  }
 }
--- src/Form/Trick/VideoType.php
+++ src/Form/Trick/VideoType.php
@@ -3,22 +3,27 @@
 namespace App\Form\Trick;
 
 use App\Entity\Video;
+use Symfony\Component\Form\FormView;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class VideoType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options): void
   {
     $builder
-      ->add('link', TextType::class, [
+      ->add('link', TextareaType::class, [
         'label' => "Lien de la vidéo",
         'block_prefix' => 'link_video',
         'required' => false
       ]);
   }
+
+
 
   public function configureOptions(OptionsResolver $resolver): void
   {
