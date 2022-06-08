1b805d486f496b005b8b5f5e2012bc41cfa4b8f3
Florimond Jouffroy <florimond.jouffroy@gmail.com>
2022-06-08 10:25:47 +0200

    Delete : file php cs fixer

--- src/Controller/Admin/Comment/IndexController.php
+++ src/Controller/Admin/Comment/IndexController.php
@@ -4,18 +4,16 @@ namespace App\Controller\Admin\Comment;
 
 use App\Entity\Comment;
 use App\Repository\CommentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Common\Collections\ArrayCollection;
-use PhpParser\Node\Expr\FuncCall;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IndexController extends AbstractController
 {
     #[Route('/admin/comments', name: 'admin_comments')]
     public function index(CommentRepository $commentRepository): Response
     {
-
         $comments = new ArrayCollection($commentRepository->findAll());
 
         $validComments = $comments->filter(function (Comment $comment) {
